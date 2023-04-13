09282a055f6078d2bd717a1a0bf354e77571ae71
Joshua Kim <joshuaki2552@gmail.com>
2023-04-13 13:05:35 -0500

    Added half of digital chalkboard assignment

---
+++ digital_chalk/lastitem.js
@@ -0,0 +1,15 @@
+function lastItem(input) {
+    var temp = [];
+    if (Array.isArray(input)) {
+        for (let i = 0; i < input.length; i++) {
+            for (let j = 0; j < input.length; j++) {
+                if (input[i] < input[j]) {
+                    temp = input[j];
+                    input[j] = input[i];
+                    input[i] = temp;
+                }
+            }
+        }
+        return input[input.length - 1];
+    }
+}
