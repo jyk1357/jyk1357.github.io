73fa46d2ba0a20224271e311b3a6a721dd4fa1e3
Joshua Kim <joshuaki2552@gmail.com>
2023-04-30 21:05:34 -0500

    Public Access to Google Map page

---
+++ assignments/javascript/a4map.js
@@ -0,0 +1,27 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Chicago
+  const position = { lat: 41.8781, lng: -87.6298 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Chicago
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Chicago
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Chicago",
+  });
+}
+
+initMap();
